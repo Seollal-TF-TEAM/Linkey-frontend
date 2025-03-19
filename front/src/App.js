811aa2fe815fca9f3d9d7fb9b4e57314ddc6f059
nahong_c <evangelion14125@gmail.com>
2025-03-19 14:10:18 +0900

    chore : react 예시

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,20 @@
-
-import './App.css';
-import GitHubLoginPage from "./auth/login/page.tsx";
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello') // 상대 경로로 변경
+      .then(response => response.text())
+      .then(data => setMessage(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
-      <GitHubLoginPage/>
+    <div>
+      <h1>React App</h1>
+      <p>Spring Boot Response: {message}</p>
+    </div>
   );
 }
 
