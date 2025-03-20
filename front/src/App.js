b771851718db196b52acaf52e67492eeaa1b6e37
CooolRyan <74342019+CooolRyan@users.noreply.github.com>
2025-03-20 10:27:33 +0900

    feat : mantine 적용 시도

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,17 @@
-
 import './App.css';
 import GitHubLoginPage from "./auth/login/page.tsx";
+import ProjectPage from "./project/depth1/page.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-      <GitHubLoginPage/>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<GitHubLoginPage />} />
+                <Route path="/project" element={<ProjectPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
