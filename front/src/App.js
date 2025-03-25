9fc8fda8c50d4298f2395941a99918c896e30040
nahong_c <evangelion14125@gmail.com>
2025-03-25 23:29:19 +0900

    fix : /callback 404페이지  수정

--- front/src/App.js
+++ front/src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import GitHubLoginPage from './auth/login/page.tsx';
-import ProjectMainPage from './project/main/page.tsx'; // ProjectPage 대신 명확히 ProjectMainPage로
-import TeamDepth1Page from './team/depth1/page.tsx'; // TeamPage로 사용 가능
+import ProjectMainPage from './project/main/page.tsx';
+import TeamDepth1Page from './team/depth1/page.tsx';
 import ProjectDetailPage from './project/[projectId]/page.tsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SprintPage from './sprint/[sprintId]/page.tsx';
 import NotFoundPage from './error/404Page.tsx';
 import withAuth from './components/withAuth.tsx';
-import MainPage from './main/page.tsx'; // 누락된 MainPage 임포트
-import CreateTeamPage from './team/createTeam/page.tsx'; // 누락된 CreateTeamPage 임포트
-import ProjectDepth1Page from './project/depth1/page.tsx'; // 누락된 ProjectDepth1Page 임포트
+import MainPage from './main/page.tsx';
+import CreateTeamPage from './team/createTeam/page.tsx';
+import ProjectDepth1Page from './project/depth1/page.tsx';
 
 // 보호된 페이지에 withAuth 적용
 const ProtectedMainPage = withAuth(MainPage);
-const ProtectedProjectDetailPage = withAuth(ProjectDetailPage); // ProjectPage 대신 ProjectDetailPage
+const ProtectedProjectDetailPage = withAuth(ProjectDetailPage);
 const ProtectedSprintPage = withAuth(SprintPage);
 const ProtectedCreateTeamPage = withAuth(CreateTeamPage);
 const ProtectedTeamDepth1Page = withAuth(TeamDepth1Page);
@@ -26,6 +26,7 @@ function App() {
             <Routes>
                 {/* 로그인 페이지 */}
                 <Route path="/" element={<GitHubLoginPage />} />
+                <Route path="/callback" element={<GitHubLoginPage />} /> {/* 콜백 경로 추가 */}
 
                 {/* 보호된 페이지 */}
                 <Route path="/main" element={<ProtectedMainPage />} />
