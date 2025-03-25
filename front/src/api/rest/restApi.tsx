1d88ade00e4d5a56923009f6f3484b09ea6f7856
nahong_c <evangelion14125@gmail.com>
2025-03-25 11:50:06 +0900

    test : BASE_URL 수정

--- front/src/api/rest/restApi.tsx
+++ front/src/api/rest/restApi.tsx
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api'; // 실제 Spring Boot 서버 주소
+const BASE_URL = 'https://linkey.co.kr/api'; // 실제 Spring Boot 서버 주소
 
 export async function getProjects() {
-    const response = await axios.get(`${BASE_URL}/api/getUserProjects`);
+    const response = await axios.get(`${BASE_URL}/getUserProjects`);
     return response.data;
 }
 
 export async function getProject() {
-    const response = await axios.get(`${BASE_URL}/api/getUserProject`);
+    const response = await axios.get(`${BASE_URL}/getUserProject`);
     return response.data;
 }
 
 export async function createProject(project) {
-    const response = await axios.post(`${BASE_URL}/api/createProject`, project);
+    const response = await axios.post(`${BASE_URL}/createProject`, project);
     return response.data;
 }
